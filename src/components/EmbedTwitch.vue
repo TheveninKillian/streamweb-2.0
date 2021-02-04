ce9e47dd5bac36fc3629a1594cb73cb30832d4d0
Killian <thevenin.killian@gmail.com>
2021-02-04 18:04:55 +0100

    create new component EmbedTwitch

---
+++ src/components/EmbedTwitch.vue
@@ -0,0 +1,43 @@
+<template>
+  <iframe src="https://player.twitch.tv/?channel=mastersnakou&parent=localhost" allowfullscreen="true"></iframe>
+
+  <div class="twitch-details">
+    <div class="twitch-details__logo">{{ twitch.logo }}</div>
+    <div class="twitch-details__title">{{ twitch.title }}</div>
+    <div class="twitch-details__game">{{ twitch.game }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'EmbedTwitch',
+    data() {
+      return {
+        twitch: {
+          logo: 'logo',
+          title: 'title',
+          game: 'game'
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  iframe{
+    position: absolute;
+    top: 75px;
+    
+    height: 32%;
+    width: 100%;
+
+    border: none;
+  }
+
+  .twitch-details{
+    position: absolute;
+    top: calc(32% + 75px);
+
+    color: #fff;
+  }
+</style>
