404a4dc3d97921905e80c75afbb6b73e5ea5df29
Killian <thevenin.killian@gmail.com>
2021-02-08 15:22:20 +0100

    create new component: Button social

---
+++ src/components/Social.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="btn-social__twitch">
+    <a href="http://www.twitch.tv/mastersnakou"><i class="lab la-twitch"></i></a>
+  </div>
+
+  <div class="btn-social__twitter">
+    <a href="https://twitter.com/MasterSnakou"><i class="lab la-twitter"></i></a>
+  </div>
+
+  <div class="btn-social__facebook">
+    <a href="https://www.facebook.com/MasterSnakou"><i class="lab la-facebook-f"></i></a>
+  </div>
+
+  <div class="btn-social__youtube">
+    <a href="https://www.youtube.com/user/MasterSnakou"><i class="lab la-youtube"></i></a>
+  </div>
+
+  <div class="btn-social__instagram">
+    <a href="https://www.instagram.com/MasterSnakou"><i class="lab la-instagram"></i></a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Social'
+  }
+</script>
