0cf433d823d92b5bcc1aaeb4a411321b3d3a7157
Killian <thevenin.killian@gmail.com>
2021-02-09 16:11:37 +0100

    update indent

--- src/components/FeedTwitter.vue
+++ src/components/FeedTwitter.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="wrapper-twitter" :class="{active: activeFeed}">
+  <div class="wrapper-twitter" :class="{ active: activeFeed }">
     <div class="wrapper-twitter__feed">
-      <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/MasterSnakou"></a>
+      <a
+        class="twitter-timeline"
+        data-theme="dark"
+        href="https://twitter.com/MasterSnakou"
+      ></a>
     </div>
 
     <div class="wrapper-twitter__btn" @click="revealFeed">
@@ -11,81 +15,81 @@
 </template>
 
 <script>
-  export default {
-    name: "FeedTwitter",
-    data(){
-      return{
-        activeFeed: false
+export default {
+  name: "FeedTwitter",
+  data() {
+    return {
+      activeFeed: false,
+    };
+  },
+  methods: {
+    revealFeed() {
+      if (this.activeFeed === false) {
+        this.activeFeed = true;
+      } else {
+        this.activeFeed = false;
       }
     },
-    methods: {
-      revealFeed(){
-        if(this.activeFeed === false){
-          this.activeFeed = true
-        }else{
-          this.activeFeed = false
-        }
-      }
-    }
-  }
+  },
+};
 </script>
 
 <style lang="scss">
-  @import '../assets/variables';
+@import "../assets/variables";
 
-  .wrapper-twitter{
-    position: absolute;
-    top: 5%;
-    left: -360px;
-    z-index: 10;
+.wrapper-twitter {
+  position: absolute;
+  top: 5%;
+  left: -360px;
+  z-index: 10;
 
-    display: flex;
-    align-items: center;
-    width: 360px;
-    height: 90%;
+  display: flex;
+  align-items: center;
+  width: 360px;
+  height: 90%;
 
-    background-color: $colorTwitter;
+  background-color: $colorTwitter;
 
-    transition: left .3s;
+  transition: left 0.3s;
 
-    &.active{
-      left: 0;
-    }
-
-    &__feed{
-      width: 95%;
-      margin: auto;
-      height: 98%;
-      overflow: auto;
-
-      &::-webkit-scrollbar {
-        display: none;
-      }
-    }
-
-    &__btn{
-      position: absolute;
-      top: 35px;
-      right: -50px;
-
-      width: 50px;
-      height: 50px;
-      line-height: 50px;
-
-      background-color: $colorTwitter;
-      border-radius: 0 10px 10px 0;
-      color: #fff;
-
-      font-size: 35px;
-      text-align: center;
-
-      cursor: pointer;
-    }
+  &.active {
+    left: 0;
   }
 
-  @media screen and (max-width: 1024px) and (orientation: portrait){
-    .wrapper-twitter{
+  &__feed {
+    width: 95%;
+    margin: auto;
+    height: 98%;
+    overflow: auto;
+
+    &::-webkit-scrollbar {
       display: none;
     }
   }
+
+  &__btn {
+    position: absolute;
+    top: 35px;
+    right: -50px;
+
+    width: 50px;
+    height: 50px;
+    line-height: 50px;
+
+    background-color: $colorTwitter;
+    border-radius: 0 10px 10px 0;
+    color: #fff;
+
+    font-size: 35px;
+    text-align: center;
+
+    cursor: pointer;
+  }
+}
+
+@media screen and (max-width: 1024px) and (orientation: portrait) {
+  .wrapper-twitter {
+    display: none;
+  }
+}
 </style>
--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,27 +1,37 @@
 <template>
   <div class="btn-social__twitch">
-    <a href="http://www.twitch.tv/mastersnakou"><i class="lab la-twitch"></i></a>
+    <a href="http://www.twitch.tv/mastersnakou"
+      ><i class="lab la-twitch"></i
+    ></a>
   </div>
 
   <div class="btn-social__twitter">
-    <a href="https://twitter.com/MasterSnakou"><i class="lab la-twitter"></i></a>
+    <a href="https://twitter.com/MasterSnakou"
+      ><i class="lab la-twitter"></i
+    ></a>
   </div>
 
   <div class="btn-social__facebook">
-    <a href="https://www.facebook.com/MasterSnakou"><i class="lab la-facebook-f"></i></a>
+    <a href="https://www.facebook.com/MasterSnakou"
+      ><i class="lab la-facebook-f"></i
+    ></a>
   </div>
 
   <div class="btn-social__youtube">
-    <a href="https://www.youtube.com/user/MasterSnakou"><i class="lab la-youtube"></i></a>
+    <a href="https://www.youtube.com/user/MasterSnakou"
+      ><i class="lab la-youtube"></i
+    ></a>
   </div>
 
   <div class="btn-social__instagram">
-    <a href="https://www.instagram.com/MasterSnakou"><i class="lab la-instagram"></i></a>
+    <a href="https://www.instagram.com/MasterSnakou"
+      ><i class="lab la-instagram"></i
+    ></a>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Social'
-  }
+export default {
+  name: "Social",
+};
 </script>
